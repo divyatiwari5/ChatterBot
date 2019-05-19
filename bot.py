02fe8f7e048f01c7070ab40145db66d254eebf90
Divya <divya@mlabs.in>
2019-05-19 14:13:54 +0530

    Last commit

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import UbuntuCorpusTrainer
+from chatterbot.logic import logic_adapter
 
 
 chatbot = ChatBot('Divya',
-                  storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
                   logic_adapters=['chatterbot.logic.BestMatch', 'chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.TimeLogicAdapter'],
                   input_adapter='chatterbot.input.TerminalAdapter',
                   output_adapter='chatterbot.output.TerminalAdapter',
-                  database='chatterbotdb',  # Enter database name here (default: chatterbot-database)
+                  database='netdb'
                   )
 
+chatbot.set_trainer(ListTrainer)
+chatbot.train(['Hi', 'Hello'])
 
 while True:
     try:
